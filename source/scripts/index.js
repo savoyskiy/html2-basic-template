72fc4a72cc48a22c301412b4d44446d7c6e9d4e0
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-01-19 22:18:37 +0300

    Добавляет хедер проекта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+/* Работа меню в мобильном варианте */
+const headerButtom = document.querySelector('.header__button');
+const headerMainMenu = document.querySelector('.header__main-menu');
+
+headerButtom.addEventListener('click', () => {
+  headerMainMenu.classList.toggle('header__main-menu--hidden');
+  headerButtom.classList.toggle('header__button--open');
+});
