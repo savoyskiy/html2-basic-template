606ef57d550ecefd26465ff5be13c27f1d92a534
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-01-25 02:18:01 +0300

    Дорабатывает пагинацию каталога

---
+++ source/scripts/catalog-pagination.js
@@ -0,0 +1,61 @@
+/* пагинация в каталоге */
+const previousButton = document.querySelector('.pagination__link--previous');
+const nextButton = document.querySelector('.pagination__link--next');
+const linkList = document.querySelectorAll('a.pagination__link');
+const pagination = document.querySelector('.pagination');
+
+let current = 1; /* начальное значение установлено 1 для отображения как в макете */
+
+const changePagination = () => {
+  if (current <= 0) {
+    // previousButton.disabled = true;
+    // nextButton.disabled = false;
+    previousButton.classList.add('pagination__link--hidden');
+    nextButton.classList.remove('pagination__link--hidden');
+  } else if (current >= linkList.length - 1) {
+    // nextButton.disabled = true;
+    // previousButton.disabled = false;
+    nextButton.classList.add('pagination__link--hidden');
+    previousButton.classList.remove('pagination__link--hidden');
+  } else {
+    // nextButton.disabled = false;
+    // previousButton.disabled = false;
+    nextButton.classList.remove('pagination__link--hidden');
+    previousButton.classList.remove('pagination__link--hidden');
+  }
+
+  linkList.forEach((elem) => {
+    elem.classList.remove('pagination__link--current');
+    linkList[current].classList.add('pagination__link--current');
+  });
+};
+
+const onNextButtonClick = () => {
+  nextButton.addEventListener('click', () => {
+    current++;
+    changePagination();
+  });
+};
+
+const onPreviousButtonClick = () => {
+  previousButton.addEventListener('click', () => {
+    current--;
+    changePagination();
+  });
+};
+
+linkList.forEach((element, index) => {
+  element.dataset.id = index;
+});
+
+const onlinkListClick = () => {
+  pagination.addEventListener('click', (evt) => {
+    if (evt.target.tagName === 'A') {
+      evt.preventDefault();
+      current = +evt.target.dataset.id;
+      changePagination();
+    }
+  });
+};
+
+export { onNextButtonClick, onPreviousButtonClick, onlinkListClick };
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,7 @@
 import { onMobileMenuButtonClick } from './mobile-menu.js';
 import { onButtonPreviousClick, onButtonNextClick, onSliderPaginationClick } from './hero-slider.js';
 import { setPriceInputsValues, setPriceMinSliderValue, setPriceMaxSliderValue, resetSliderValues } from './price-slider.js';
+import { onNextButtonClick, onPreviousButtonClick, onlinkListClick } from './catalog-pagination.js';
 
 /* Работа меню в мобильном варианте */
 onMobileMenuButtonClick();
@@ -16,3 +17,9 @@ setPriceInputsValues();
 setPriceMinSliderValue();
 setPriceMaxSliderValue();
 resetSliderValues();
+
+/* пагинация в каталоге */
+/* реализовано только переключение ссылок и их стилей */
+onNextButtonClick();
+onPreviousButtonClick();
+onlinkListClick();
